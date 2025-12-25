5ff1b2ee92cbdb90f905b0eadb32bf44aacb6ebe
J Rogel-Salazar <j.rogel@ortus.co.uk>
2025-12-25 13:28:45 +0000

    Updating Xcode project

---
+++ Xcode_Project/CaliforniaPricer/CaliforniaPricer/CaliforniaPricerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaliforniaPricerApp.swift
+//  CaliforniaPricer
+//
+//  Created by J. Rogel on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CaliforniaPricerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode_Project/CaliforniaPricer/CaliforniaPricer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaliforniaPricer
+//
+//  Created by J. Rogel on 25/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
